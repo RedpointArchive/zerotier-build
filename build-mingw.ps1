1b307d2c6d378a7d35ba10c3b40e5bd38a1293d9
June Rhodes <jrhodes@redpoint.games>
2020-02-02 15:17:08 +1100

    Enable debug logs

--- build-mingw.ps1
+++ build-mingw.ps1
@@ -40,8 +40,11 @@ try {
     RemoveMSVCFlags $PSScriptRoot\libzt\CMakeLists.txt
     RemoveMSVCFlags $PSScriptRoot\libzt\zto\java\CMakeLists.txt
     FileReplace $PSScriptRoot\libzt\include\libzt.h "#include <cstdint>" "#include <stdint.h>`n#if defined(GOLANG_CGO)`n#define bool int`n#endif"
+    FileReplace $PSScriptRoot\libzt\include\libztDebug.h "#if defined(_WIN32)" "#if !defined(_WIN32)"
+    FileReplace $PSScriptRoot\libzt\include\libztDebug.h "#define LIBZT_DEBUG_HPP" "#define LIBZT_DEBUG_HPP`n#define LIBZT_DEBUG 1`n#define LIBZT_TRACE 1`n"
     FileAppend $PSScriptRoot\libzt\CMakeLists.txt 'add_library(ztunified STATIC ${lwip_src_glob} ${zto_src_glob} "${ZTO_SRC_DIR}/ext/http-parser/http_parser.c" ${libzt_src_glob})'
-    $env:CXXFLAGS="-fpermissive"
+    $env:CFLAGS="-DLIBZT_DEBUG=1 -DLIBZT_TRACE=1"
+    $env:CXXFLAGS="-fpermissive -DLIBZT_DEBUG=1 -DLIBZT_TRACE=1"
     cmake -G "MinGW Makefiles" ..\libzt
     mingw32-make ztunified
 } finally {
--- gotest/main.go
+++ gotest/main.go
@@ -7,19 +7,20 @@ import "C"
 import (
 	"fmt"
 	"os"
-	"time"
 )
 
 func main() {
 	os.MkdirAll("zt_config/path", 0644)
 
+	C.zts_set_service_port(9102)
+    
 	fmt.Println("starting zerotier")
-	res := C.zts_start(C.CString("zt_config/path"), 0)
+	res := C.zts_startjoin(C.CString("zt_config/path"), 0x8056c2e21c000001)
 	if res != 0 {
 		fmt.Println("zerotier can't start")
 		os.Exit(1)
 	}
-
+/*
 	fmt.Println("joining network")
 	res = C.zts_join(0xb6079f73c66e4c60)
 	if res != 0 {
@@ -38,7 +39,7 @@ func main() {
 	if res != 0 {
 		fmt.Println("zerotier can't leave network")
 		os.Exit(1)
-	}
+	}*/
 
 	fmt.Println("done")
 	C.zts_stop()
